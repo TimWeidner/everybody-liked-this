9981e61d90a6b3116a749abae268b2814efe5268
Tim Weidner <weidner.tim@hotmail.de>
2024-03-04 22:04:10 +0100

    first commit

---
+++ scripts/everybody-liked-this.js
@@ -0,0 +1,100 @@
+let socket;
+let module;
+
+class LikeOrDislike extends Application {
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      id: "elt-like",
+      title: "Approval",
+      template: `modules/everybody-liked-this/templates/like-or-dislike.hbs`,
+      width: 100,
+      height: 63,
+      resizable: false,
+    });
+  }
+}
+
+class Status extends Application {
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      id: "elt-status",
+      title: "Messages",
+      template: `modules/everybody-liked-this/templates/status.hbs`,
+      width: 300,
+      height: 100,
+      top: 80,
+      left: 130,
+      resizable: false,
+      minimizable: false,
+    });
+  }
+}
+
+const getCurrentUser = () => game.users.current;
+
+const sendApproval = (approval) => {
+  const user = getCurrentUser();
+  socket.executeForEveryone("approval", user.name, approval);
+};
+
+const cleanWindowClass = (application) => {
+  const app = application.element[0];
+  app.style.boxShadow = "none";
+  app.style.background = "none";
+  app.querySelector(".window-content").classList.remove("window-content");
+  app.querySelector(".window-header").style.backgroundColor =
+    "rgba(0, 0, 0, 0.5)";
+  app.querySelector(".window-header").style.border = "0";
+  Array.from(app.querySelectorAll("button")).forEach((el) =>
+    app.querySelector(".window-header").appendChild(el)
+  );
+  app.querySelector(".close").remove();
+};
+
+Hooks.once("socketlib.ready", () => {
+  socket = socketlib.registerModule("everybody-liked-this");
+  socket.register("approval", approval);
+  console.log("The socket is ready to be used.");
+});
+
+Hooks.once("init", () => {
+  console.log("Everybody Liked This | Initializing the module.");
+  module = game.modules.get("everybody-liked-this");
+  //module.likeOrDislike = new LikeOrDislike();
+  //module.likeOrDislike.render(true);
+  module.status = new Status();
+  module.status.render(true);
+});
+
+Hooks.once("ready", async () => {
+  //cleanWindowClass(module.likeOrDislike);
+  //module.likeOrDislike.element[0].style.width = "100px";
+  cleanWindowClass(module.status);
+  Array.from(
+    module.status.element[0].querySelectorAll(".module-control")
+  ).forEach((el) =>
+    el.addEventListener("click", (event) => {
+      sendApproval(event.currentTarget.dataset.approval === "true");
+    })
+  );
+});
+
+const approval = (user, approval) => {
+  let newElement = document.createElement("div");
+  //Add transition styles to the new element
+  newElement.style.transition = "opacity 1s";
+  newElement.style.paddingLeft = "8px";
+
+  newElement.innerHTML = `<p>${user} ${
+    approval ? "approves" : "disapproves"
+  }.</p>`;
+  module.status.element[0].querySelector(".statusbox").appendChild(newElement);
+
+  //Set timer to first fade out and then remove the element after 5 seconds
+  setTimeout(() => {
+    newElement.style.opacity = 0;
+    setTimeout(() => {
+      newElement.remove();
+    }, 1000);
+  }, 5000);
+};
